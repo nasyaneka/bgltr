c7d68425b3ec4e1a738cbd36c4aa559e7eb1c6ab
nasyaneka <96820549+nasyaneka@users.noreply.github.com>
2022-01-03 10:12:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from app.salary import calculate_salary
+from app.bd.people import get_employees
+
+
+if __name__ == '__main__':
+    get_employees()
+
+if __name__ == '__main__':
+    calculate_salary()
+
