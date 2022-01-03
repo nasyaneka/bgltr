77e77857bbc975e3cf2ac867084e9411c8df74e0
nasyaneka <96820549+nasyaneka@users.noreply.github.com>
2022-01-03 10:05:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+
+if __name__ == '__main__':
+    get_employees()
+
+if __name__ == '__main__':
+    calculate_salary()
+
---
+++ salary.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+def calculate_salary():
+    current_datetime = datetime.now()
+    print(current_datetime)
+    print('calculate salary end')
