8235f3848fdfef3e37991a0e6f787b30daf4cae4
nasyaneka <96820549+nasyaneka@users.noreply.github.com>
2022-01-25 21:30:08 +0300

    Update iter.py

--- iter.py
+++ iter.py
@@ -1,5 +1,4 @@
 
-
 class Iterr(list):
     def __iter__(self):
         return self
@@ -20,18 +19,4 @@ for i in listt:
 
 
 
-print(*(' '.join(m) for m in members), sep="\n")
-
-
-
-cont = 0
-    cont1 = 0
-    while list[cont] in list:
-        cont2 = cont1
-        while list[cont][cont2] in list[cont]:
-            yield list[cont][cont2]
-            cont2 += 1
-            continue
-        cont += 1
-        continue
 
