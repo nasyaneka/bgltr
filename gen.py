a76da3ec6f66bb4c93481d021717a91180c7427b
nasyaneka <96820549+nasyaneka@users.noreply.github.com>
2022-02-04 19:40:34 +0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,21 +1,17 @@
-listt = [
+nested_list = [
 	['a', 'b', 'c'],
 	['d', 'e', 'f'],
 	[1, 2, None],
 ]
 
-def Mygen(list):
-	cont = 0
-	cont1 = 0
-	while list[cont] in list:
-		cont2 = cont1
-		while list[cont][cont2] in list[cont]:
-			yield list[cont][cont2]
-			cont2 += 1
-			continue
-		cont += 1
-		continue
 
-for i in Mygen(listt):
-	print(i)
+def flat_generator(listt):
+	for lists in listt:
+		cnt1 = 0
+		while cnt1 < len(listt):
+			yield lists[cnt1]
+			cnt1 += 1
 
+
+for item in flat_generator(nested_list):
+	print(item)
