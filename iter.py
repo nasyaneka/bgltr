5c93bfba9ebdf4b9d76752ea56e0379750b9b5f5
nasyaneka <96820549+nasyaneka@users.noreply.github.com>
2022-02-04 19:41:57 +0300

    Update iter.py

--- iter.py
+++ iter.py
@@ -1,22 +1,37 @@
+listt = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],
+]
 
-class Iterr(list):
+
+class FlatIterator:
+
+    def __init__(self, some_list):
+        self.main_list = some_list
+        self.lenn = len(some_list)
+
     def __iter__(self):
+        self.main_list_cursor = 0
+        self.nested_list_cursor = -1
         return self
 
     def __next__(self):
-        for i in self:
-            return i
 
-listt = Iterr([
-	['a', 'b', 'c'],
-	['d', 'e', 'f', 'h', False],
-	[1, 2, None],
-])
+        self.nested_list_cursor += 1
+        if self.nested_list_cursor == len(self.main_list[self.main_list_cursor]):
+            self.main_list_cursor += 1
+            self.nested_list_cursor = 0
 
-
-for i in listt:
-    print(i)
+        if self.main_list_cursor == len(self.main_list):
+            raise StopIteration
+        return self.main_list[self.main_list_cursor][self.nested_list_cursor]
 
 
+for item in FlatIterator(listt):
+    print(item)
 
+flat_list = [item for item in FlatIterator(listt)]
+
+print(flat_list)
 
